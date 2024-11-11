331dca0bc51d0c742659b10742cac4691e1acd80
king0jin <et0709@naver.com>
2024-11-11 11:49:49 +0900

    project generate

---
+++ src/main/java/naver/et0709/jenkinsconnect/JenkinsconnectApplication.java
@@ -0,0 +1,13 @@
+package naver.et0709.jenkinsconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsconnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/naver/et0709/jenkinsconnect/JenkinsconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.et0709.jenkinsconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
